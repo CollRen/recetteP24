08f2bba2c0f2449928e86a56ecfdaea1273032a5
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-04-19 14:06:29 -0400

    Traduire les messages d'introduction

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,9 +1,9 @@
 <?php
 return [
     "text_welcome" => "Bienvenue",
-    "text_welcome_title" => "Bienvenue dans l'application ToDo List",
-    "text_welcome_paragraph" => "<p class='lead'>Il s'agit d'une simple application de liste de tâches construite avec Laravel et Bootstrap.</p><p>Commencez par créer votre première tâche !</p>",
-    'text_welcome_btn' => "Aller à la liste de tâches",
+    "text_welcome_title" => "Bienvenue dans votre application de recettes",
+    "text_welcome_paragraph" => "<p class='lead'>Il s'agit simplement d'une application de recettes construite avec Laravel et Tailwind.</p><p>Commencez par créer votre première recette !</p>",
+    'text_welcome_btn' => "Aller à la liste des recettes",
     'text_login_msg' => "Merci de vous connecter pour continuer.",
     'text_copryright' => "Tous droits réservés."
 ];
