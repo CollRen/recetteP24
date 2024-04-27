49fa994da7670ef5c0c9e9b73c5a77ef2ce8c7b5
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-04-27 14:11:38 -0400

    Ajouter Category Factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -13,7 +13,7 @@ use App\Models\Category;
 class CategoryFactory extends Factory
 {
 
-    
+
     /**
      * Define the model's default state.
      *
@@ -21,20 +21,47 @@ class CategoryFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'category' => json_encode([$this->faker->randomElement(
-                [
-                  "house",
-                  "flat", 
-                  "apartment", 
-                  "room", "shop", 
-                  "lot", "garage"
-                ]
-             )
-        ]),
 
+        $genericObject = new  \stdClass();
+        $genericObject->en = 'Desert';
+        $genericObject->fr = 'Dessert';
+
+        $plat = new  \stdClass();
+        $plat->en = 'Main Dishes';
+        $plat->fr = 'Repas principal';
+
+        $lunch = new  \stdClass();
+        $lunch->en = 'Lunch';
+        $lunch->fr = 'Diner';
+
+        $dinner = new  \stdClass();
+        $dinner->en = 'Dinner';
+        $dinner->fr = 'Souper';
+
+        $collation = new  \stdClass();
+        $collation->en = 'Main Dishes';
+        $collation->fr = 'Repas principal';
+
+        $apero = new  \stdClass();
+        $apero->en = 'Apero';
+        $apero->fr = 'Apero';
+
+        $dejeuner = new  \stdClass();
+        $dejeuner->en = 'Breakfast';
+        $dejeuner->fr = 'Déjeuner';
+
+        $arrayCategory = [
+            $genericObject, $plat, $lunch, $dinner, $collation, $apero, $dejeuner
+        ];
+/*         if (!isset($i)) $i = 1;
+        function ($i) {
+            $i++;
+        }; */
+
+        // $i n'augmente pas de valeur... changer manuellement la valeur et repasser le php artisan db:seed
+        $i = 6;
+        return [
+            'category' => [$arrayCategory[$i]],
         ];
     }
-
-    
 }
