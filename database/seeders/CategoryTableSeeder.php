4cfc9aa4aff180edacc25094d4337342c5b2a949
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-05-11 14:50:59 -0400

    Mettre à jour le category seeder

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -13,11 +13,54 @@ class CategoryTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory(1)->create();
+        // Category::factory(1)->create();
 
-        
+        $genericObject = new \stdClass();
+        $genericObject->en = 'Desert';
+        $genericObject->fr = 'Dessert';
+
+        $plat = new \stdClass();
+        $plat->en = 'Main Dishes';
+        $plat->fr = 'Repas principal';
+
+        $lunch = new \stdClass();
+        $lunch->en = 'Lunch';
+        $lunch->fr = 'Diner';
+
+        $dinner = new \stdClass();
+        $dinner->en = 'Dinner';
+        $dinner->fr = 'Souper';
+
+        $collation = new \stdClass();
+        $collation->en = 'Collation';
+        $collation->fr = 'Collation';
+
+        $apero = new \stdClass();
+        $apero->en = 'Apero';
+        $apero->fr = 'Apéro';
+
+        $dejeuner = new \stdClass();
+        $dejeuner->en = 'Breakfast';
+        $dejeuner->fr = 'Déjeuner';
+
+        $arrayCategory = [
+            $genericObject,
+            $plat,
+            $lunch,
+            $dinner,
+            $collation,
+            $apero,
+            $dejeuner
+        ];
+
+        foreach ($arrayCategory as $el) {
+
+            Category::create([
+                "category" => $el
+            ]);
+        }
 
     }
 
- 
+
 }
