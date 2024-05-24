79802028304d4d3bad29d734be61e57485404579
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-05-24 13:50:25 -0400

    Ajouter les images aux recettes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,9 +7,9 @@ $annee = date('Y');
         <div class="container mx-auto px-4">
             <div class="flex flex-wrap text-left lg:text-left">
                 <div class="w-full lg:w-6/12 px-4">
-                    <h4 class="text-3xl fonat-semibold text-clr-pri1">Des commentaires&nbsp;?</h4>
+                    <h4 class="text-3xl fonat-semibold text-clr-pri1">@lang('Have Comments')&nbsp;?</h4>
                     <h5 class="text-lg mt-0 mb-2 text-clr-pri1">
-                        Rejoignez-nous sur nos plateformes sociales.
+                        @lang('Let\'s Chat')
                     </h5>
                     <div class="mt-6 lg:mb-0 mb-6">
                         <button
@@ -32,7 +32,7 @@ $annee = date('Y');
                 <div class="w-full lg:w-6/12 px-4">
                     <div class="flex flex-wrap items-top mb-6">
                         <div class="w-full lg:w-4/12 px-4 ml-auto">
-                            <span class="block uppercase text-clr-pri2 text-sm font-semibold mb-2">Liens utililes</span>
+                            <span class="block uppercase text-clr-pri2 text-sm font-semibold mb-2">@lang('Usefull Links')</span>
                             <ul class="list-unstyled">
                                 <li>
                                     <a class="text-clr-pri1 hover:text-clr-pri1 font-semibold block pb-2 text-sm"
@@ -50,15 +50,15 @@ $annee = date('Y');
                             </ul>
                         </div>
                         <div class="w-full lg:w-4/12 px-4">
-                            <span class="block uppercase text-clr-pri2 text-sm font-semibold mb-2">Autres ressources</span>
+                            <span class="block uppercase text-clr-pri2 text-sm font-semibold mb-2">Ressources</span>
                             <ul class="list-unstyled">
                                 <li>
                                     <a class="text-clr-pri1 hover:text-clr-pri1 font-semibold block pb-2 text-sm"
-                                        href="#">Termes &amp; Conditions</a>
+                                        href="#">@lang('Termes')</a>
                                 </li>
                                 <li>
                                     <a class="text-clr-pri1 hover:text-clr-pri1 font-semibold block pb-2 text-sm"
-                                        href="#">Vie privée</a>
+                                        href="#">@lang('Privacy')</a>
                                 </li>
                                 <li>
                                     <a class="text-clr-pri1 hover:text-clr-pri1 font-semibold block pb-2 text-sm"
@@ -73,11 +73,11 @@ $annee = date('Y');
             <div class="flex flex-wrap items-center md:justify-between justify-center">
                 <div class="w-full md:w-4/12 px-4 mx-auto text-center">
                     <div class="text-sm text-clr-pri2 font-semibold py-1">
-                        Copyright © <span id="get-current-year">@php
+                        @lang('Copyright') © <span id="get-current-year">@php
                             echo $annee;
                         @endphp</span><a
                             href="#"
-                            class="text-clr-pri2 hover:text-gray-800" target="_blank"> Créé par
+                            class="text-clr-pri2 hover:text-gray-800" target="_blank"> @lang('Create by')
                             <a href="https://www.creative-tim.com?ref=njs-profile"
                                 class="text-clr-pri2 hover:text-clr-pri1">RDM WEB</a>.
                     </div>
