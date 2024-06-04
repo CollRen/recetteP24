1583097e657cb3c43d35daf15ad5a3135a77734f
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-06-04 11:25:19 -0400

    Ajouter un fond blanc au dropdown du menu admin

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -35,14 +35,14 @@
 
           <!-- Dropdown menu Language-->
           <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
-              class="btn btn-primary text-gray-900 hover:bg-clr-pale1 focus:ring-3 focus:outline-none focus:ring-clr-sec2 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-clr-sec2 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="btn btn-primary text-gray-900 hover:bg-clr-fonce1 focus:ring-3 focus:outline-none focus:ring-clr-sec2 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-clr-sec2 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               type="button">Admin<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                   fill="none" viewBox="0 0 10 6">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="m1 1 4 4 4-4" />
               </svg>
           </button>
-          <div id="dropdown" class="hidden divide-clr-pale1 rounded-lg shadow w-44 dark:bg-gray-700">
+          <div id="dropdown" class="hidden divide-clr-pale1 rounded-lg w-44 bg-white ">
               <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
 
                   <li>
